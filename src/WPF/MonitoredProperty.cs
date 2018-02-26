b9467954b11e0b94c75e29b6769a4e1611c1c725
Yuriy Karpenko <molodoy@li.ru>
2018-02-26 18:47:56 +0200

    update WPF classes

--- src/WPF/MonitoredProperty.cs
+++ src/WPF/MonitoredProperty.cs
@@ -20,6 +20,11 @@ namespace IT.WPF
 		/// Срабатывает при изменении значения
 		/// </summary>
 		public event EventHandler<EventArgs<T>> ValueChanged;
+		/// <summary>
+		/// Вызывается после изменения свойства
+		/// </summary>
+		/// <param name="v">Новое значение</param>
+		protected virtual void OnValueChanged(T v) => ValueChanged?.Invoke(this, new EventArgs<T>(v));
 
 
 		/// <summary>
@@ -40,6 +45,7 @@ namespace IT.WPF
 			}
 		}
 		private T _value;
+
 
 		/// <summary>
 		/// Конструктор
@@ -63,18 +69,6 @@ namespace IT.WPF
 
 
 		/// <summary>
-		/// Вызывается после изменения свойства
-		/// </summary>
-		/// <param name="v">Новое значение</param>
-		protected virtual void OnValueChanged(T v)
-		{
-			if (this.ValueChanged != null)
-			{
-				this.ValueChanged(this, new EventArgs<T>(v));
-			}
-		}
-
-		/// <summary>
 		/// Вывод значения
 		/// </summary>
 		/// <returns></returns>
@@ -82,5 +76,11 @@ namespace IT.WPF
 		{
 			return string.Format("MonitoredProperty<{0}> (Value = {1})", typeof(T).Name, this.Value);
 		}
+
+		/// <summary>
+		/// Неявное преобразование данного класса в тип его значения (T Value)
+		/// </summary>
+		/// <param name="v"></param>
+		public static implicit operator T (MonitoredProperty<T> v) => v.Value;
 	}
 }
