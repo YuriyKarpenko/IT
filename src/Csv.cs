1d24a84e0e61cc8e83ae76e73587668f6de1ffe2
Unknown <molodoy@li.ru>
2018-03-01 13:47:42 +0200

    update csv

--- src/Csv.cs
+++ src/Csv.cs
@@ -14,16 +14,12 @@ namespace IT
 	/// <summary>
 	/// Базовый класс для работы с основными операциями формата Csv
 	/// </summary>
-	/// <typeparam name="TItemProperty">Тип свойств полей, как правило <see cref="PropertyInfo"/> или <see cref="PropertyDescriptor"/> </typeparam>
-	/// <typeparam name="T">Тип обрабатываемых данных</typeparam>
-	public abstract class CsvBase<TItemProperty, T> : ILog
+	public abstract class CsvBase : ILog
 	{
 		/// <summary>
 		/// Дефолтный разделитель полей
 		/// </summary>
-		public const string DEFAULT_SEPARATOR = ";";
-
-		#region Properties
+		public static string DEFAULT_SEPARATOR { get; set; } = ";";
 
 		/// <summary>
 		/// Разделитель полей
@@ -32,17 +28,165 @@ namespace IT
 		public string Separator { get; set; }
 
 		/// <summary>
-		/// Набор свойств указанного типа
+		/// ctor
 		/// </summary>
-		public TItemProperty[] ItemPropertyes { get; set; }
+		public CsvBase() => Separator = DEFAULT_SEPARATOR;
+	}
+
+	/// <summary>
+	/// Класс для работы с основными операциями формата Csv. 
+	/// </summary>
+	/// <typeparam name="T">Тип обрабатываемых данных</typeparam>
+	public class CsvReader<T> : CsvBase
+	{
+		private readonly Func<string[], T> _Creator;
+
+		/// <summary>
+		/// Запускается сразу после прочтения файла
+		/// </summary>
+		public Func<IEnumerable<string>, IEnumerable<string>> FilterSource { get; set; }
+
+		/// <summary>
+		/// Прочитанные сущности
+		/// </summary>
+		public T[] Items { get; set; }
+
+		/// <summary>
+		/// ctor
+		/// </summary>
+		/// <param name="creator"></param>
+		public CsvReader(Func<string[], T> creator)
+		{
+			Contract.NotNull(creator, nameof(creator));
+			_Creator = creator;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <param name="splitCount"></param>
+		/// <param name="creator"></param>
+		/// <returns></returns>
+		public IEnumerable<T> Load(string fileName, int splitCount, Func<string[], T> creator = null)
+		{
+			try
+			{
+				creator = creator ?? _Creator;
+				Items = null;
+				if (File.Exists(fileName))
+				{
+					var lines = File.ReadAllLines(fileName);
+					Items = (FilterSource?.Invoke(lines) ?? lines)
+						.Select(i => creator(i.Split(Separator.ToCharArray(), splitCount, StringSplitOptions.RemoveEmptyEntries)))
+						.ToArray();
+				}
+			}
+			catch (Exception ex)
+			{
+				this.Error(ex);
+			}
+
+			return Items;
+		}
+	}
+
+	/// <summary>
+	/// Упрощенный класс для работы с основными операциями формата Csv. 
+	/// </summary>
+	public class CsvReader : CsvReader<string[]>
+	{
+		/// <summary>
+		/// ctor
+		/// </summary>
+		public CsvReader() : base(i => i) { }
+	}
+
+	/// <summary>
+	/// Класс для работы с основными операциями формата Csv. Понимает атрибуты DisplayNameAttribute и DescriptionAttribute
+	/// </summary>
+	/// <typeparam name="T">Тип обрабатываемых данных</typeparam>
+	public class CsvWriter<T> : CsvBase
+	{
+		#region static
+
+		/// <summary>
+		/// Создает Csv с указанными полями (пересечение) и порядком следования
+		/// </summary>
+		/// <param name="fields">Позволяет ограничить/фильтровать состав полей, указанными</param>
+		/// <returns></returns>
+		public static CsvWriter<T> Create(string[] fields)
+		{
+			var pis = fields
+				.Join(typeof(T).GetProperties(), i => i, i => i.Name, (s, p) => p)
+				.ToArray();
+			var csv = new CsvWriter<T>(pis);
+			return csv;
+		}
+
+		///// <summary>
+		///// Метод запиви набора данных в выходной потокововый объект
+		///// </summary>
+		///// <param name="list">Исходный набор данных</param>
+		///// <param name="outStream"></param>
+		///// <param name="includeHeader"></param>
+		//public static void ToStream(IEnumerable<T> list, Stream outStream, bool includeHeader = true)
+		//{
+		//	Csv<T>.UsingCsv(csv => csv.Serialize(list, outStream, includeHeader));
+		//}
+
+		///// <summary>
+		///// Метод запиви набора данных в выходной потокововый объект
+		///// </summary>
+		///// <param name="list">Исходный набор данных</param>
+		//public static Stream ToStream(IEnumerable<T> list)
+		//{
+		//	Stream str = null;
+		//	Csv<T>.UsingCsv(csv => str = csv.ToStream(list, true));
+		//	return str;
+		//}
+
+		/// <summary>
+		/// Предоставляет готовый экземпляр для удобной работы с ним
+		/// </summary>
+		/// <param name="act"></param>
+		/// <param name="existingItem"></param>
+		public static void UsingCsv(Action<CsvWriter<T>> act, CsvWriter<T> existingItem = null)
+		{
+			Logger.ToLogFmt(null, TraceLevel.Verbose, null, "()");
+			try
+			{
+				var csv = existingItem ?? new CsvWriter<T>();
+				act(csv);
+			}
+			catch (Exception ex)
+			{
+				Logger.ToLogFmt(null, TraceLevel.Error, ex, "()");
+				throw;
+			}
+		}
 
 		#endregion
+
+
+		private Func<T, string> actGetRow;
+
+		#region Properties
+
+		/// <summary>
+		/// Набор свойств указанного типа
+		/// </summary>
+		public PropertyInfo[] ItemPropertyes { get; set; }
+
+		#endregion
+
+		#region ctors
 
 		/// <summary>
 		/// Конструктор
 		/// </summary>
 		/// <param name="itemPropertyes">Набор свойств указанного типа</param>
-		public CsvBase(TItemProperty[] itemPropertyes)
+		public CsvWriter(PropertyInfo[] itemPropertyes)
 		{
 			Contract.NotNull(itemPropertyes, "itemPropertyes");
 
@@ -50,6 +194,29 @@ namespace IT
 
 			this.ItemPropertyes = itemPropertyes;
 		}
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		public CsvWriter() : this(typeof(T).GetProperties()) { }
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		/// <param name="pis">Набор свойств указанного типа</param>
+		/// <param name="getRow">Метод получения строки данных указанного объекта</param>
+		public CsvWriter(PropertyInfo[] pis, Func<T, string> getRow = null) : this(pis)
+		{
+			this.actGetRow = getRow;
+		}
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		/// <param name="getRow">Метод получения строки данных указанного объекта</param>
+		public CsvWriter(Func<T, string> getRow) : this()
+		{
+			this.actGetRow = getRow;
+		}
+
+		#endregion
 
 
 		/// <summary>
@@ -92,6 +259,33 @@ namespace IT
 		/// Метод запиви набора данных в выходной потокововый объект
 		/// </summary>
 		/// <param name="list">Исходный набор данных</param>
+		/// <param name="outStream"></param>
+		/// <param name="includeHeader"></param>
+		public void Serialize(IEnumerable<T> list, Stream outStream, bool includeHeader = true)
+		{
+			Contract.NotNull(outStream, "outStream");
+			var sw = new StreamWriter(outStream, Encoding.UTF8);
+			this.Write(list, sw, includeHeader);
+		}
+
+		/// <summary>
+		/// Создает и заполняет MemoryStream
+		/// </summary>
+		/// <param name="list"></param>
+		/// <param name="includeHeader"></param>
+		/// <returns></returns>
+		public Stream ToStream(IEnumerable<T> list, bool includeHeader = true)
+		{
+			var ms = new MemoryStream();
+			this.Serialize(list, ms, includeHeader);
+			ms.Position = 0;
+			return ms;
+		}
+
+		/// <summary>
+		/// Метод запиви набора данных в выходной потокововый объект
+		/// </summary>
+		/// <param name="list">Исходный набор данных</param>
 		/// <param name="tw"></param>
 		/// <param name="includeHeader">Записывать ли заголовок</param>
 		public void Write(IEnumerable<T> list, TextWriter tw, bool includeHeader)
@@ -121,12 +315,41 @@ namespace IT
 		}
 
 
+
+		/// <summary>
+		/// Метод получения заголовка
+		/// </summary>
+		/// <returns></returns>
+		protected string GetHeaderCore()
+		{
+			if (this.ItemPropertyes.Length == 0)
+				return "value";
+			else
+				return this.UsingProps(p => p.GetNameFromAttributes(p.Name));
+		}
+
+		/// <summary>
+		/// Метод получения строки данных указанного объекта
+		/// </summary>
+		/// <param name="v"></param>
+		/// <returns></returns>
+		protected string GetRowCore(T v)
+		{
+			if (this.actGetRow != null)
+				return this.actGetRow(v);
+
+			if (this.ItemPropertyes.Length == 0)
+				return v.ToString();
+			else
+				return this.UsingProps(p => p.GetValue(v, null)?.ToString() ?? string.Empty);
+		}
+
 		/// <summary>
 		/// Вспомогательный метод получения строки из предоставляемых полей
 		/// </summary>
 		/// <param name="getPropertyValue"></param>
 		/// <returns></returns>
-		protected virtual string UsingProps(Func<TItemProperty, string> getPropertyValue)
+		protected virtual string UsingProps(Func<PropertyInfo, string> getPropertyValue)
 		{
 			try
 			{
@@ -140,171 +363,6 @@ namespace IT
 				this.Error(ex, "()");
 				throw;
 			}
-		}
-
-		/// <summary>
-		/// Метод получения заголовка
-		/// </summary>
-		/// <returns></returns>
-		protected abstract string GetHeaderCore();
-
-		/// <summary>
-		/// Метод получения строки данных указанного объекта
-		/// </summary>
-		/// <param name="v"></param>
-		/// <returns></returns>
-		protected abstract string GetRowCore(T v);
-	}
-
-	/// <summary>
-	/// Класс для работы с основными операциями формата Csv. Понимает атрибуты DisplayNameAttribute и DescriptionAttribute
-	/// </summary>
-	/// <typeparam name="T">Тип обрабатываемых данных</typeparam>
-	public class Csv<T> : CsvBase<PropertyInfo, T>
-	{
-		#region static
-
-		/// <summary>
-		/// Создает Csv с указанными полями (пересечение) и порядком следования
-		/// </summary>
-		/// <param name="fields"></param>
-		/// <returns></returns>
-		public static Csv<T> Create(string[] fields)
-		{
-			var pis = fields
-				.Join(typeof(T).GetProperties(), i => i, i => i.Name, (s, p) => p)
-				.ToArray();
-			var csv = new Csv<T>(pis);
-			return csv;
-		}
-
-		///// <summary>
-		///// Метод запиви набора данных в выходной потокововый объект
-		///// </summary>
-		///// <param name="list">Исходный набор данных</param>
-		///// <param name="outStream"></param>
-		///// <param name="includeHeader"></param>
-		//public static void ToStream(IEnumerable<T> list, Stream outStream, bool includeHeader = true)
-		//{
-		//	Csv<T>.UsingCsv(csv => csv.Serialize(list, outStream, includeHeader));
-		//}
-
-		///// <summary>
-		///// Метод запиви набора данных в выходной потокововый объект
-		///// </summary>
-		///// <param name="list">Исходный набор данных</param>
-		//public static Stream ToStream(IEnumerable<T> list)
-		//{
-		//	Stream str = null;
-		//	Csv<T>.UsingCsv(csv => str = csv.ToStream(list, true));
-		//	return str;
-		//}
-
-		/// <summary>
-		/// Предоставляет готовый экземпляр для удобной работы с ним
-		/// </summary>
-		/// <param name="act"></param>
-		/// <param name="existingItem"></param>
-		public static void UsingCsv(Action<Csv<T>> act, Csv<T> existingItem = null)
-		{
-			Logger.ToLogFmt(null, TraceLevel.Verbose, null, "()");
-			try
-			{
-				var csv = existingItem ?? new Csv<T>();
-				act(csv);
-			}
-			catch (Exception ex)
-			{
-				Logger.ToLogFmt(null, TraceLevel.Error, ex, "()");
-				throw;
-			}
-		}
-
-		#endregion
-
-
-		private Func<T, string> actGetRow;
-
-		#region ctors
-
-		/// <summary>
-		/// Конструктор
-		/// </summary>
-		public Csv() : base(typeof(T).GetProperties()) { }
-		/// <summary>
-		/// Конструктор
-		/// </summary>
-		/// <param name="pis">Набор свойств указанного типа</param>
-		/// <param name="getRow">Метод получения строки данных указанного объекта</param>
-		public Csv(PropertyInfo[] pis, Func<T, string> getRow = null) : base(pis) 
-		{
-			this.actGetRow = getRow;
-		}
-		/// <summary>
-		/// Конструктор
-		/// </summary>
-		/// <param name="getRow">Метод получения строки данных указанного объекта</param>
-		public Csv(Func<T, string> getRow) : this() 
-		{
-			this.actGetRow = getRow;
-		}
-
-		#endregion
-
-
-		/// <summary>
-		/// Метод запиви набора данных в выходной потокововый объект
-		/// </summary>
-		/// <param name="list">Исходный набор данных</param>
-		/// <param name="outStream"></param>
-		/// <param name="includeHeader"></param>
-		public void Serialize(IEnumerable<T> list, Stream outStream, bool includeHeader = true)
-		{
-			Contract.NotNull(outStream, "outStream");
-			var sw = new StreamWriter(outStream, Encoding.UTF8);
-			this.Write(list, sw, includeHeader);
-		}
-
-		/// <summary>
-		/// Создает и заполняет MemoryStream
-		/// </summary>
-		/// <param name="list"></param>
-		/// <param name="includeHeader"></param>
-		/// <returns></returns>
-		public Stream ToStream(IEnumerable<T> list, bool includeHeader = true)
-		{
-			var ms = new MemoryStream();
-			this.Serialize(list, ms, includeHeader);
-			ms.Position = 0;
-			return ms;
-		}
-
-		/// <summary>
-		/// Метод получения заголовка
-		/// </summary>
-		/// <returns></returns>
-		protected override string GetHeaderCore()
-		{
-			if (this.ItemPropertyes.Length == 0)
-				return "value";
-			else
-				return this.UsingProps(p => p.GetNameFromAttributes(p.Name));
-		}
-
-		/// <summary>
-		/// Метод получения строки данных указанного объекта
-		/// </summary>
-		/// <param name="v"></param>
-		/// <returns></returns>
-		protected override string GetRowCore(T v)
-		{
-			if (this.actGetRow != null)
-				return this.actGetRow(v);
-
-			if (this.ItemPropertyes.Length == 0)
-				return v.ToString();
-			else
-				return this.UsingProps(p => p.GetValue(v, null)?.ToString() ?? "");
 		}
 	}
 }
